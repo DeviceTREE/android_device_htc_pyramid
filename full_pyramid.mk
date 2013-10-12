d188aec8f22f2fef49dc8c78814b8e4dab0f5032
CodeKill13 <codekill13@gmail.com>
2013-10-11 22:49:52 -0400

    Bring in the last of a dying breed

---
+++ full_pyramid.mk
@@ -0,0 +1,21 @@
+
+
+# Release name
+PRODUCT_RELEASE_NAME := pyramid
+
+# Bootanimation
+TARGET_SCREEN_HEIGHT := 960
+TARGET_SCREEN_WIDTH := 540
+
+
+# Inherit device configuration
+$(call inherit-product, device/htc/pyramid/device_pyramid.mk)
+
+# Device naming
+PRODUCT_DEVICE := pyramid
+PRODUCT_NAME := full_pyramid
+PRODUCT_BRAND := htc
+PRODUCT_MODEL := HTC Sensation
+PRODUCT_MANUFACTURER := HTC
+
+
